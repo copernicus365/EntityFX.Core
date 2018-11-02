2e7ce286e2e7d8706deb07d490da175e6cd227a5
Nicholas Petersen <copernicus365@gmail.com>
2018-11-02 18:46:07 -0400

    For EFQueryableExtensions, changed the extension names from old way (e.g. "ToArrayAsync_EFCore") to new: "ToArrayEFCoreAsync". New comment description on this class.

--- EntityFX.Core/src/QueryableExtensions.cs
+++ EntityFX.Core/src/QueryableExtensions.cs
@@ -9,23 +9,29 @@ using Microsoft.EntityFrameworkCore;
 namespace EntityFX.QueryableExtensions
 {
 	/// <summary>
-	/// The purpose of this is for cases where client code (that is just 
-	/// working with IQueryable, NOT even anything else) access IQueryable
-	/// sources sometimes where the source is from the old EntityFramework, 
-	/// and other cases where it is Entity Framework Core. The old way of getting
-	/// the `ToArrayAsync` etc methods required the namespace System.Data, whereas
-	/// the new EFCore simply requires the `Microsoft.EntityFrameworkCore` namespace.
-	/// So to reference each of these causes a conflict if not calling from the right
-	/// one. This class therefore provides intermediates to those extension methods
-	/// for EFCore, and it's important to note that this requires a special namespace
-	/// different from the rest of this project: <see cref="EntityFX.QueryableExtensions"/>.
-	/// Set that as a using only when there is going to be a possible mixture between
-	/// old and new. Doing so then will make these explicitly for EFCore extension 
-	/// methods available.
+	/// Both the old Entity Framework and the new Entity Framework Core expose async
+	/// extension methods on IQueryable, such as: `ToArrayAsync`, `FirstOrDefaultAsync`, 
+	/// and so forth. The old way required a using statement as follows: 
+	/// `using System.Data.Entity;`, whereas
+	/// the new way simply uses the same base namespace for the rest of EFCore to be set:
+	/// `using Microsoft.EntityFrameworkCore;`. The problem arises, however, when a single
+	/// code file has IQueryable methods exposed (typically through repository interfaces)
+	/// which have a backing source from both of these, the old and the new EF. Doing this causes
+	/// exceptions. For instance, calling `ToArrayAsync` on an IQueryable whose backing store
+	/// was the new EFCore, will throw an exception if the `ToArrayAsync` actually came from your
+	/// `System.Data.Entity;` using statement.
+	/// <para />
+	/// What this class allows you to do is to get around this problem, by keeping the 
+	/// `using System.Data.Entity;` reference, but then by adding a using statement 
+	/// to this class, namely: `using EntityFX.QueryableExtensions;` You still have to know
+	/// which queryables came from EFCore, but then you can call one of the methods herein,
+	/// such as `ToArrayEFCoreAsync`, and so forth. Since the name is different, there is no 
+	/// conflict. You still have to be careful to use only these methods on IQueryables 
+	/// that came from EFCore, but this way, everyone plays happily in the same playground.
 	/// </summary>
 	public static class EFQueryableExtensions
 	{
-		public static Task<TSource> FirstOrDefaultAsync_EFCore<TSource>(
+		public static Task<TSource> FirstOrDefaultEFCoreAsync<TSource>(
 			this IQueryable<TSource> source, 
 			Expression<Func<TSource, bool>> predicate, 
 			CancellationToken cancellationToken = default(CancellationToken))
@@ -34,7 +40,7 @@ namespace EntityFX.QueryableExtensions
 				source, predicate, cancellationToken);
 		}
 
-		public static Task<TSource[]> ToArrayAsync_EFCore<TSource>(
+		public static Task<TSource[]> ToArrayEFCoreAsync<TSource>(
 			this IQueryable<TSource> source,
 			CancellationToken cancellationToken = default(CancellationToken))
 		{
@@ -42,7 +48,7 @@ namespace EntityFX.QueryableExtensions
 				source, cancellationToken);
 		}
 
-		public static Task<List<TSource>> ToListAsync_EFCore<TSource>(
+		public static Task<List<TSource>> ToListEFCoreAsync<TSource>(
 			this IQueryable<TSource> source,
 			CancellationToken cancellationToken = default(CancellationToken))
 		{
